211aaa531e66b7b88c44fc85d9cd86bf86873435
andrewkryvoruchko <andrew.kryvoruchko@gmail.com>
2020-04-18 16:08:02 +0300

    index.html

--- shered.js
+++ shered.js
@@ -6,6 +6,10 @@ var NumberMenu = document.querySelector(".number__menu")
 var IconLibra = document.querySelector(".down__rigth__image_one")
 var IconLike = document.querySelector(".down__rigth__image_two")
 var IconShop = document.querySelector(".down__rigth__image_tree")
+var CityButton = document.querySelector(".city__button")
+var CityMenu = document.querySelector(".city")
+var SignIn = document.querySelector(".rigth-sign_in__button")
+var SignInMenu = document.querySelector(".sign_in")
 
 OpenButtonCatalog.addEventListener('click', function(){
     BlackDrop.classList.add("open");
@@ -18,12 +22,26 @@ BlackDrop.addEventListener("click", function() {
     CatalogMenu.classList.remove("open");
     NumberMenu.classList.remove("open");
     BlackDrop.style.zIndex = "20"
+    CityMenu.classList.remove("open")
+    SignInMenu.classList.remove("open")
 })
 
 NumberButton.addEventListener("click", function() {
     BlackDrop.classList.add("open");
     BlackDrop.style.zIndex = "30"
     NumberMenu.classList.add("open");
+})
+
+CityButton.addEventListener("click", function() {
+    BlackDrop.classList.add("open");
+    BlackDrop.style.zIndex = "30"
+    CityMenu.classList.add("open")
+})
+
+SignIn.addEventListener("click", function() {
+    SignInMenu.classList.add("open")
+    BlackDrop.classList.add("open");
+    BlackDrop.style.zIndex = "30"
 })
 
 
